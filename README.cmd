441b0bc349b9fba566fceadb2f72df5770574d31
makramdtcc <33176731+makramdtcc@users.noreply.github.com>
2020-12-05 20:22:46 -0500

    Update README.cmd

--- README.cmd
+++ README.cmd
@@ -1,3 +1,14 @@
-## This is demo file
-
-End of file
+echo "# demo-repo" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/makramdtcc/demo-repo.git
+git push -u origin main
+                
+…or push an existing repository from the command line
+git remote add origin https://github.com/makramdtcc/demo-repo.git
+git branch -M main
+git push -u origin main
+…or import code from another repository
+You can initialize this repository with code from a Subversion, Mercurial, or TFS project.
